5542ae7b20f0102c195da503f1de7bbe860af2f5
serejja <serejja@gmail.com>
2015-05-15 16:52:46 +0300

    Added config parsing

--- dropwizard/src/scala/ly/stealth/mesos/logging/Scheduler.scala
+++ dropwizard/src/scala/ly/stealth/mesos/logging/Scheduler.scala
@@ -2,22 +2,92 @@ package ly.stealth.mesos.logging
 
 import java.util
 
+import ly.stealth.mesos.logging.Util.Str
 import org.apache.log4j._
 import org.apache.mesos.Protos._
 import org.apache.mesos.{MesosSchedulerDriver, SchedulerDriver}
-import Util.Str
+
 import scala.collection.JavaConversions._
 
 object Scheduler extends org.apache.mesos.Scheduler {
   private val logger = Logger.getLogger(this.getClass)
   private var driver: SchedulerDriver = null
 
+  private var runningInstances = 0
+  private var config: SchedulerConfig = null
+
+  def parseConfig(args: Array[String]) {
+    val parser = new scopt.OptionParser[SchedulerConfig]("scheduler") {
+      opt[String]('m', "master").required().text("Mesos Master addresses.").action {
+        (value, config) =>
+          config.copy(master = value)
+      }
+
+      opt[String]('u', "user").required().text("Mesos user.").action {
+        (value, config) =>
+          config.copy(user = value)
+      }
+
+      opt[Int]('i', "instances").required().text("Number of tasks to run.").action {
+        (value, config) =>
+          config.copy(instances = value)
+      }
+
+      opt[String]('h', "artifact.host").optional().text("Binding host for artifact server.").action {
+        (value, config) =>
+          config.copy(artifactServerHost = value)
+      }
+
+      opt[Int]('p', "artifact.port").optional().text("Binding port for artifact server.").action {
+        (value, config) =>
+          config.copy(artifactServerPort = value)
+      }
+
+      opt[String]('e', "executor").required().text("Executor file name.").action {
+        (value, config) =>
+          config.copy(executor = value)
+      }
+
+      opt[Double]('c', "cpu.per.task").optional().text("CPUs per task.").action {
+        (value, config) =>
+          config.copy(cpuPerTask = value)
+      }
+
+      opt[Double]('r', "mem.per.task").optional().text("Memory per task.").action {
+        (value, config) =>
+          config.copy(memPerTask = value)
+      }
+
+      opt[String]('s', "schema.registry").required().text("Avro Schema Registry url.").action {
+        (value, config) =>
+          config.copy(schemaRegistryUrl = value)
+      }
+
+      opt[String]('b', "broker.list").required().text("Comma separated list of brokers for producer.").action {
+        (value, config) =>
+          config.copy(brokerList = value)
+      }
+
+      opt[String]('t', "topic").required().text("Topic to produce transformed data to.").action {
+        (value, config) =>
+          config.copy(topic = value)
+      }
+    }
+
+    parser.parse(args, SchedulerConfig()) match {
+      case Some(c) => this.config = c
+      case None => sys.exit(1)
+    }
+  }
+
   def main(args: Array[String]) {
+    parseConfig(args)
+
     val frameworkBuilder = FrameworkInfo.newBuilder()
-    frameworkBuilder.setUser(args(1)) //TODO use flags
+    frameworkBuilder.setUser(config.user)
     frameworkBuilder.setName("Dropwizard LogLine Transform Framework")
 
-    val driver = new MesosSchedulerDriver(Scheduler, frameworkBuilder.build, args(0)) //TODO use flags
+    val driver = new MesosSchedulerDriver(Scheduler, frameworkBuilder.build, config.master)
 
     Runtime.getRuntime.addShutdownHook(new Thread() {
       override def run() {
@@ -58,6 +128,12 @@ object Scheduler extends org.apache.mesos.Scheduler {
 
   override def statusUpdate(driver: SchedulerDriver, status: TaskStatus) {
     logger.info("[statusUpdate] " + Str.taskStatus(status))
+
+    status.getState match {
+      case TaskState.TASK_LOST | TaskState.TASK_FINISHED | TaskState.TASK_FAILED |
+        TaskState.TASK_KILLED | TaskState.TASK_ERROR => this.runningInstances -= 1
+      case _ =>
+    }
   }
 
   override def frameworkMessage(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, data: Array[Byte]) {
@@ -72,3 +148,8 @@ object Scheduler extends org.apache.mesos.Scheduler {
     logger.info("[executorLost] executor:" + Str.id(executorId.getValue) + " slave:" + Str.id(slaveId.getValue) + " status:" + status)
   }
 }
+
+private case class SchedulerConfig(master: String = "", user: String = "root", instances: Int = 1,
+                                   artifactServerHost: String = "master", artifactServerPort: Int = 6666,
+                                   executor: String = "", cpuPerTask: Double = 0.2, memPerTask: Double = 256,
+                                   schemaRegistryUrl: String = "", brokerList: String = "", topic: String = "")
