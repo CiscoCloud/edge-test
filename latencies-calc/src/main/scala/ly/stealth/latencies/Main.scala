1e5a5d04a80775d7a321438bb8bd04c00a815753
ilyutov <ivanlyutov@gmail.com>
2015-05-15 14:52:10 +0300

    Spark job draft

---
+++ latencies-calc/src/main/scala/ly/stealth/latencies/Main.scala
@@ -0,0 +1,100 @@
+package ly.stealth.latencies
+
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+import com.datastax.spark.connector.rdd
+import io.confluent.kafka.serializers.KafkaAvroDecoder
+import _root_.kafka.serializer.DefaultDecoder
+import org.apache.avro.generic.GenericData.Record
+import org.apache.avro.generic.{GenericData, GenericRecord}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka._
+import org.apache.spark.streaming._
+
+object Main extends App {
+  val parser = new scopt.OptionParser[AppConfig]("latencies-calc") {
+    head("Latencies calculation job", "1.0")
+    opt[String]("topic") unbounded() required() action { (value, config) =>
+      config.copy(topic = value)
+    } text ("Topic to read data from")
+    opt[String]("broker.list") unbounded() required() action { (value, config) =>
+      config.copy(brokerList = value)
+    } text ("Comma separated string of host:port")
+    opt[String]("schema.registry.url") unbounded() required() action { (value, config) =>
+      config.copy(schemaRegistryUrl = value)
+    } text ("Schema registry URL")
+    checkConfig { c =>
+      if (c.topic.isEmpty || c.brokerList.isEmpty) {
+        failure("You haven't provided all required parameters")
+      } else {
+        success
+      }
+    }
+  }
+  val appConfig = parser.parse(args, AppConfig()) match {
+    case Some(c) => c
+    case None => sys.exit(1)
+  }
+
+  val sparkConfig = new SparkConf().setAppName("latencies-calc").set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+  val ssc = new StreamingContext(sparkConfig, Seconds(1))
+  ssc.checkpoint("spark-analysis")
+
+  val cassandraConnector = CassandraConnector(sparkConfig)
+  cassandraConnector.withSessionDo(session => {
+    session.execute("CREATE KEYSPACE IF NOT EXISTS spark_analysis WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}")
+    session.execute("CREATE TABLE IF NOT EXISTS spark_analysis.aggregations(eventName text, second int, operation text, value int, PRIMARY KEY(eventName, second))")
+  })
+
+  val consumerConfig = Map("metadata.broker.list" -> appConfig.brokerList,
+    "auto.offset.reset" -> "smallest",
+    "schema.registry.url" -> appConfig.schemaRegistryUrl)
+
+  start(consumerConfig, appConfig.topic)
+
+  ssc.start()
+  ssc.awaitTermination()
+
+  def start(consumerConfig: Map[String, String], topic: String) = {
+    val stream = KafkaUtils.createDirectStream[Array[Byte], Any, DefaultDecoder, KafkaAvroDecoder](ssc, consumerConfig, Set(topic))
+    stream.persist()
+    calculateAverages(stream, "second", 10)
+    calculateAverages(stream, "second", 30)
+    calculateAverages(stream, "minute", 1)
+    calculateAverages(stream, "minute", 5)
+    calculateAverages(stream, "minute", 10)
+    calculateAverages(stream, "minute", 15)
+  }
+
+  def calculateAverages(stream: InputDStream[(Array[Byte], Any)], durationUnit: String, durationValue: Long) = {
+    stream.window(windowDuration(durationUnit, durationValue)).map(value => {
+      val record = value.asInstanceOf[GenericRecord]
+      import scala.collection.JavaConversions._
+      val timings = record.get("timings").asInstanceOf[GenericData.Array[Record]]
+      timings.combinations(2).map(entry => {
+        (entry.head.get("eventName").asInstanceOf[String] + "-" + entry.last.get("eventName").asInstanceOf[String], 
+          entry.last.get("value").asInstanceOf[Long] - entry.head.get("value").asInstanceOf[Long])
+      }).toList
+    }).reduce((acc, value) => {
+      acc ++ value
+    }).map(entry => {
+      entry.groupBy(_._1).map { case (key, values) => {
+        val timings = values.map(_._2)
+        Event(key, 0, "avg%d%s".format(durationValue, durationUnit), timings.sum / timings.size)
+      }
+      }
+    }).foreachRDD(rdd => {
+      rdd.saveToCassandra("spark_analysis", "aggregations")
+    })
+  }
+  
+  def windowDuration(unit: String, durationValue: Long): Duration = unit match {
+    case "second" => Seconds(durationValue) 
+    case "minute" => Minutes(durationValue)
+  }
+}
+
+case class Event(eventName: String, second: Long, operation: String, value: Long)
+
+case class AppConfig(topic: String = "", brokerList: String = "", schemaRegistryUrl: String = "")
