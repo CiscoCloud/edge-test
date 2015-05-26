a8c0eb734a4168b46e1d73f4ec85fb0618c8677f
serejja <serejja@gmail.com>
2015-05-26 13:23:00 +0300

    Moved common executor behaviour to ExecutorBase to simplify implementation of new Executors

--- dropwizard/src/scala/ly/stealth/mesos/logging/Executor.scala
+++ dropwizard/src/scala/ly/stealth/mesos/logging/Executor.scala
@@ -1,14 +1,10 @@
 package ly.stealth.mesos.logging
 
-import ly.stealth.mesos.logging.Util.Str
-import org.apache.log4j.Logger
+import org.apache.mesos.MesosExecutorDriver
 import org.apache.mesos.Protos._
-import org.apache.mesos.{ExecutorDriver, MesosExecutorDriver, Protos}
 
-object Executor extends org.apache.mesos.Executor {
-  private var shutdownFlag = false
+object Executor extends ExecutorBase {
   private var config: ExecutorConfig = null
-  private val lock = new Object()
 
   def parseConfig(args: Array[String]) {
     val parser = new scopt.OptionParser[ExecutorConfig]("executor") {
@@ -40,75 +36,8 @@ object Executor extends org.apache.mesos.Executor {
     System.exit(status)
   }
 
-  private val logger = Logger.getLogger(this.getClass)
-
-  override def registered(driver: ExecutorDriver, executorInfo: ExecutorInfo, framework: FrameworkInfo, slave: SlaveInfo) {
-    logger.info("[registered] framework:" + Str.framework(framework) + " slave:" + Str.slave(slave))
-  }
-
-  override def shutdown(driver: ExecutorDriver) {
-    logger.info("[shutdown]")
-
-    lock.synchronized {
-      shutdownFlag = true
-      lock.notifyAll()
-    }
-  }
-
-  override def disconnected(driver: ExecutorDriver) {
-    logger.info("[disconnected]")
-  }
-
-  override def killTask(driver: ExecutorDriver, id: TaskID) {
-    logger.info("[killTask] " + id.getValue)
-
-    lock.synchronized {
-      shutdownFlag = true
-      lock.notifyAll()
-    }
-  }
-
-  override def reregistered(driver: ExecutorDriver, slave: SlaveInfo) {
-    logger.info("[reregistered] " + Str.slave(slave))
-  }
-
-  override def error(driver: ExecutorDriver, message: String) {
-    logger.info("[error] " + message)
-  }
-
-  override def frameworkMessage(driver: ExecutorDriver, data: Array[Byte]) {
-    logger.info("[frameworkMessage] " + new String(data))
-  }
-
-  override def launchTask(driver: ExecutorDriver, task: TaskInfo) {
-    logger.info("[launchTask] " + Str.task(task))
-
-    val runStatus = TaskStatus.newBuilder().setTaskId(task.getTaskId).setState(Protos.TaskState.TASK_RUNNING).build
-
-    driver.sendStatusUpdate(runStatus)
-
-    new Thread {
-      override def run() {
-        var failed = false
-        try {
-          logger.info("[Started task] " + Str.task(task))
-          new ExecutorEndpoint(config).run("server", config.dropwizardConfig)
-
-          while (!shutdownFlag) {
-            lock.synchronized(lock.wait())
-          }
-        } catch {
-          case t: Throwable =>
-            logger.warn("", t)
-            failed = true
-        } finally {
-          val finishedStatus = TaskStatus.newBuilder().setTaskId(task.getTaskId)
-            .setState(if (failed) Protos.TaskState.TASK_FAILED else Protos.TaskState.TASK_FINISHED).build
-          driver.sendStatusUpdate(finishedStatus)
-          driver.stop()
-        }
-      }
-    }.start()
+  override protected def start() {
+    new ExecutorEndpoint(config).run("server", config.dropwizardConfig)
   }
 }
 
