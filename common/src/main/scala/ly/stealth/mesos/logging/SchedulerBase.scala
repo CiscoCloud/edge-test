1bea4f760560b38cab9a2fbd0defdd963e55e5cd
serejja <serejja@gmail.com>
2015-05-26 13:09:17 +0300

    Moved base Scheduler behavior to SchedulerBase to simplify implementation of other schedulers

---
+++ common/src/main/scala/ly/stealth/mesos/logging/SchedulerBase.scala
@@ -0,0 +1,114 @@
+package ly.stealth.mesos.logging
+
+import java.util
+
+import ly.stealth.mesos.logging.Util.Str
+import org.apache.log4j.Logger
+import org.apache.mesos.Protos._
+import org.apache.mesos.{Protos, Scheduler, SchedulerDriver}
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+
+abstract class SchedulerBase extends Scheduler {
+  private val logger = Logger.getLogger(this.getClass)
+
+  private var runningInstances = 0
+  private val tasks: mutable.Set[TaskID] = mutable.Set()
+
+  override def registered(driver: SchedulerDriver, id: FrameworkID, master: MasterInfo) {
+    logger.info("[registered] framework:" + Str.id(id.getValue) + " master:" + Str.master(master))
+  }
+
+  override def offerRescinded(driver: SchedulerDriver, id: OfferID) {
+    logger.info("[offerRescinded] " + Str.id(id.getValue))
+  }
+
+  override def disconnected(driver: SchedulerDriver) {
+    logger.info("[disconnected]")
+  }
+
+  override def reregistered(driver: SchedulerDriver, master: MasterInfo) {
+    logger.info("[reregistered] master:" + Str.master(master))
+  }
+
+  override def slaveLost(driver: SchedulerDriver, id: SlaveID) {
+    logger.info("[slaveLost] " + Str.id(id.getValue))
+  }
+
+  override def error(driver: SchedulerDriver, message: String) {
+    logger.info("[error] " + message)
+  }
+
+  override def statusUpdate(driver: SchedulerDriver, status: TaskStatus) {
+    logger.info("[statusUpdate] " + Str.taskStatus(status))
+
+    status.getState match {
+      case TaskState.TASK_LOST | TaskState.TASK_FINISHED | TaskState.TASK_FAILED |
+           TaskState.TASK_KILLED | TaskState.TASK_ERROR => synchronized {
+        tasks -= status.getTaskId
+        this.runningInstances -= 1
+      }
+      case _ =>
+    }
+  }
+
+  override def frameworkMessage(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, data: Array[Byte]) {
+    logger.info("[frameworkMessage] executor:" + Str.id(executorId.getValue) + " slave:" + Str.id(slaveId.getValue) + " data: " + new String(data))
+  }
+
+  override def resourceOffers(driver: SchedulerDriver, offers: util.List[Offer]) {
+    logger.info("[resourceOffers]\n" + Str.offers(offers))
+
+    synchronized {
+      if (runningInstances > config.instances) {
+        val toKill = runningInstances - config.instances
+        ((0 until toKill) zip tasks).foreach { case (index, taskId) =>
+          driver.killTask(taskId)
+        }
+      }
+    }
+
+    offers.foreach { offer =>
+      synchronized {
+        if (runningInstances < config.instances) {
+          launchTask(offer) match {
+            case Some(taskInfo) =>
+              tasks += taskInfo.getTaskId
+              runningInstances += 1
+              driver.launchTasks(util.Arrays.asList(offer.getId), util.Arrays.asList(taskInfo), Filters.newBuilder().setRefuseSeconds(1).build)
+            case None => driver.declineOffer(offer.getId)
+          }
+        }
+      }
+    }
+  }
+
+  override def executorLost(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, status: Int) {
+    logger.info("[executorLost] executor:" + Str.id(executorId.getValue) + " slave:" + Str.id(slaveId.getValue) + " status:" + status)
+  }
+
+  protected def getScalarResources(offer: Offer, name: String): Double = {
+    offer.getResourcesList.foldLeft(0.0) { (all, current) =>
+      if (current.getName == name) all + current.getScalar.getValue
+      else all
+    }
+  }
+
+  protected def getRangeResources(offer: Offer, name: String): List[Protos.Value.Range] = {
+    offer.getResourcesList.foldLeft[List[Protos.Value.Range]](List()) { case (all, current) =>
+      if (current.getName == name) all ++ current.getRanges.getRangeList
+      else all
+    }
+  }
+
+  protected def config: ConfigBase
+
+  protected def launchTask(offer: Offer): Option[TaskInfo]
+}
+
+trait ConfigBase {
+  var cpuPerTask: Double = 0.2
+  var memPerTask: Double = 256
+  var instances: Int = 1
+}
--- dropwizard/src/scala/ly/stealth/mesos/logging/Scheduler.scala
+++ dropwizard/src/scala/ly/stealth/mesos/logging/Scheduler.scala
@@ -1,23 +1,14 @@
 package ly.stealth.mesos.logging
 
-import java.util
 import java.util.UUID
 
-import ly.stealth.mesos.logging.Util.Str
-import org.apache.log4j._
 import org.apache.mesos.Protos._
-import org.apache.mesos.{MesosSchedulerDriver, Protos, SchedulerDriver}
+import org.apache.mesos.{MesosSchedulerDriver, Protos}
 
-import scala.collection.JavaConversions._
-import scala.collection.mutable
+object Scheduler extends SchedulerBase {
+  private var schedulerConfig: SchedulerConfig = null
 
-object Scheduler extends org.apache.mesos.Scheduler {
-  private val logger = Logger.getLogger(this.getClass)
-
-  private var runningInstances = 0
-  private var config: SchedulerConfig = null
-
-  private val tasks: mutable.Set[TaskID] = mutable.Set()
+  override protected def config: ConfigBase = schedulerConfig
 
   def parseConfig(args: Array[String]) {
     val parser = new scopt.OptionParser[SchedulerConfig]("scheduler") {
@@ -30,7 +21,8 @@ object Scheduler extends org.apache.mesos.Scheduler {
       }
 
       opt[Int]('i', "instances").optional().text("Number of tasks to run.").action { (value, config) =>
-        config.copy(instances = value)
+        config.instances = value
+        config
       }
 
       opt[String]('h', "artifact.host").optional().text("Binding host for artifact server.").action { (value, config) =>
@@ -46,11 +38,13 @@ object Scheduler extends org.apache.mesos.Scheduler {
       }
 
       opt[Double]('c', "cpu.per.task").optional().text("CPUs per task.").action { (value, config) =>
-        config.copy(cpuPerTask = value)
+        config.cpuPerTask = value
+        config
       }
 
       opt[Double]('r', "mem.per.task").optional().text("Memory per task.").action { (value, config) =>
-        config.copy(memPerTask = value)
+        config.memPerTask = value
+        config
       }
 
       opt[String]('s', "producer.config").required().text("Producer config file name.").action { (value, config) =>
@@ -71,7 +65,7 @@ object Scheduler extends org.apache.mesos.Scheduler {
     }
 
     parser.parse(args, SchedulerConfig()) match {
-      case Some(c) => this.config = c
+      case Some(c) => this.schedulerConfig = c
       case None => sys.exit(1)
     }
   }
@@ -79,13 +73,13 @@ object Scheduler extends org.apache.mesos.Scheduler {
   def main(args: Array[String]) {
     parseConfig(args)
 
-    new HttpServer(config).run("server", config.dropwizardConfig)
+    new HttpServer(schedulerConfig).run("server", schedulerConfig.dropwizardConfig)
 
     val frameworkBuilder = FrameworkInfo.newBuilder()
-    frameworkBuilder.setUser(config.user)
+    frameworkBuilder.setUser(schedulerConfig.user)
     frameworkBuilder.setName("Dropwizard LogLine Transform Framework")
 
-    val driver = new MesosSchedulerDriver(Scheduler, frameworkBuilder.build, config.master)
+    val driver = new MesosSchedulerDriver(Scheduler, frameworkBuilder.build, schedulerConfig.master)
 
     Runtime.getRuntime.addShutdownHook(new Thread() {
       override def run() {
@@ -97,120 +91,43 @@ object Scheduler extends org.apache.mesos.Scheduler {
     System.exit(status)
   }
 
-  override def registered(driver: SchedulerDriver, id: FrameworkID, master: MasterInfo) {
-    logger.info("[registered] framework:" + Str.id(id.getValue) + " master:" + Str.master(master))
-  }
-
-  override def offerRescinded(driver: SchedulerDriver, id: OfferID) {
-    logger.info("[offerRescinded] " + Str.id(id.getValue))
-  }
-
-  override def disconnected(driver: SchedulerDriver) {
-    logger.info("[disconnected]")
-  }
-
-  override def reregistered(driver: SchedulerDriver, master: MasterInfo) {
-    logger.info("[reregistered] master:" + Str.master(master))
-  }
-
-  override def slaveLost(driver: SchedulerDriver, id: SlaveID) {
-    logger.info("[slaveLost] " + Str.id(id.getValue))
-  }
-
-  override def error(driver: SchedulerDriver, message: String) {
-    logger.info("[error] " + message)
-  }
-
-  override def statusUpdate(driver: SchedulerDriver, status: TaskStatus) {
-    logger.info("[statusUpdate] " + Str.taskStatus(status))
-
-    status.getState match {
-      case TaskState.TASK_LOST | TaskState.TASK_FINISHED | TaskState.TASK_FAILED |
-           TaskState.TASK_KILLED | TaskState.TASK_ERROR => synchronized {
-        tasks -= status.getTaskId
-        this.runningInstances -= 1
-      }
-      case _ =>
-    }
-  }
-
-  override def frameworkMessage(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, data: Array[Byte]) {
-    logger.info("[frameworkMessage] executor:" + Str.id(executorId.getValue) + " slave:" + Str.id(slaveId.getValue) + " data: " + new String(data))
-  }
-
-  override def resourceOffers(driver: SchedulerDriver, offers: util.List[Offer]) {
-    logger.info("[resourceOffers]\n" + Str.offers(offers))
-
-    synchronized {
-      if (runningInstances > config.instances) {
-        val toKill = runningInstances - config.instances
-        ((0 until toKill) zip tasks).foreach { case (index, taskId) =>
-          driver.killTask(taskId)
-        }
-      }
+  override def launchTask(offer: Offer): Option[TaskInfo] = {
+    val cpus = getScalarResources(offer, "cpus")
+    val mems = getScalarResources(offer, "mem")
+    val ports = getRangeResources(offer, "ports")
+    val portOpt = ports.headOption.map(_.getBegin)
+    val adminPortOpt = ports.headOption.flatMap { range =>
+      val port = range.getBegin + 1
+      if (range.getEnd >= port) Some(port)
+      else None
     }
 
-    offers.foreach { offer =>
-      val cpus = getScalarResources(offer, "cpus")
-      val mems = getScalarResources(offer, "mem")
-      val ports = getRangeResources(offer, "ports")
-      val portOpt = ports.headOption.map(_.getBegin)
-      val adminPortOpt = ports.headOption.flatMap { range =>
-        val port = range.getBegin + 1
-        if (range.getEnd >= port) Some(port)
-        else None
-      }
+    if (cpus > schedulerConfig.cpuPerTask && mems > schedulerConfig.memPerTask && portOpt.nonEmpty && adminPortOpt.nonEmpty) {
+      val id = "transform-" + UUID.randomUUID().toString
+      val taskId = TaskID.newBuilder().setValue(id).build()
+      val taskInfo = TaskInfo.newBuilder().setName(taskId.getValue).setTaskId(taskId).setSlaveId(offer.getSlaveId)
+        .setExecutor(this.createExecutor(id, portOpt.get, adminPortOpt.get))
+        .addResources(Protos.Resource.newBuilder().setName("cpus").setType(Protos.Value.Type.SCALAR).setScalar(Protos.Value.Scalar.newBuilder().setValue(schedulerConfig.cpuPerTask)))
+        .addResources(Protos.Resource.newBuilder().setName("mem").setType(Protos.Value.Type.SCALAR).setScalar(Protos.Value.Scalar.newBuilder().setValue(schedulerConfig.memPerTask)))
+        .addResources(Protos.Resource.newBuilder().setName("ports").setType(Protos.Value.Type.RANGES).setRanges(
+        Protos.Value.Ranges.newBuilder().addRange(Protos.Value.Range.newBuilder().setBegin(portOpt.get).setEnd(adminPortOpt.get))
+      )).build
 
-      synchronized {
-        if (runningInstances < config.instances && cpus > config.cpuPerTask && mems > config.memPerTask && portOpt.nonEmpty && adminPortOpt.nonEmpty) {
-          val id = "transform-" + UUID.randomUUID().toString
-          val taskId = TaskID.newBuilder().setValue(id).build()
-          val taskInfo = TaskInfo.newBuilder().setName(taskId.getValue).setTaskId(taskId).setSlaveId(offer.getSlaveId)
-            .setExecutor(this.createExecutor(id, portOpt.get, adminPortOpt.get))
-            .addResources(Protos.Resource.newBuilder().setName("cpus").setType(Protos.Value.Type.SCALAR).setScalar(Protos.Value.Scalar.newBuilder().setValue(config.cpuPerTask)))
-            .addResources(Protos.Resource.newBuilder().setName("mem").setType(Protos.Value.Type.SCALAR).setScalar(Protos.Value.Scalar.newBuilder().setValue(config.memPerTask)))
-            .addResources(Protos.Resource.newBuilder().setName("ports").setType(Protos.Value.Type.RANGES).setRanges(
-            Protos.Value.Ranges.newBuilder().addRange(Protos.Value.Range.newBuilder().setBegin(portOpt.get).setEnd(adminPortOpt.get))
-          )).build
-
-          tasks += taskId
-          runningInstances += 1
-
-          driver.launchTasks(util.Arrays.asList(offer.getId), util.Arrays.asList(taskInfo), Filters.newBuilder().setRefuseSeconds(1).build)
-        } else driver.declineOffer(offer.getId)
-      }
-    }
-  }
-
-  override def executorLost(driver: SchedulerDriver, executorId: ExecutorID, slaveId: SlaveID, status: Int) {
-    logger.info("[executorLost] executor:" + Str.id(executorId.getValue) + " slave:" + Str.id(slaveId.getValue) + " status:" + status)
-  }
-
-  private def getScalarResources(offer: Offer, name: String): Double = {
-    offer.getResourcesList.foldLeft(0.0) { (all, current) =>
-      if (current.getName == name) all + current.getScalar.getValue
-      else all
-    }
-  }
-
-  private def getRangeResources(offer: Offer, name: String): List[Protos.Value.Range] = {
-    offer.getResourcesList.foldLeft[List[Protos.Value.Range]](List()) { case (all, current) =>
-      if (current.getName == name) all ++ current.getRanges.getRangeList
-      else all
-    }
+      Some(taskInfo)
+    } else None
   }
 
   private def createExecutor(id: String, port: Long, adminPort: Long): ExecutorInfo = {
-    val path = this.config.executor.split("/").last
-    val executorConfigPath = this.config.executorDropwizardConfig.split("/").last
-    val producerConfigPath = this.config.producerConfig.split("/").last
-    val cmd = s"java -Ddw.server.applicationConnectors[0].port=$port -Ddw.server.adminConnectors[0].port=$adminPort -cp ${this.config.executor} ly.stealth.mesos.logging.Executor " +
-      s"--producer.config ${this.config.producerConfig} --topic ${this.config.topic}"
+    val path = this.schedulerConfig.executor.split("/").last
+    val executorConfigPath = this.schedulerConfig.executorDropwizardConfig.split("/").last
+    val producerConfigPath = this.schedulerConfig.producerConfig.split("/").last
+    val cmd = s"java -Ddw.server.applicationConnectors[0].port=$port -Ddw.server.adminConnectors[0].port=$adminPort -cp ${this.schedulerConfig.executor} ly.stealth.mesos.logging.Executor " +
+      s"--producer.config ${this.schedulerConfig.producerConfig} --topic ${this.schedulerConfig.topic}"
     ExecutorInfo.newBuilder().setExecutorId(ExecutorID.newBuilder().setValue(id))
       .setCommand(CommandInfo.newBuilder()
-      .addUris(CommandInfo.URI.newBuilder.setValue(s"http://${this.config.artifactServerHost}:${this.config.artifactServerPort}/resource/$path"))
-      .addUris(CommandInfo.URI.newBuilder.setValue(s"http://${this.config.artifactServerHost}:${this.config.artifactServerPort}/resource/$executorConfigPath"))
-      .addUris(CommandInfo.URI.newBuilder.setValue(s"http://${this.config.artifactServerHost}:${this.config.artifactServerPort}/resource/$producerConfigPath"))
+      .addUris(CommandInfo.URI.newBuilder.setValue(s"http://${this.schedulerConfig.artifactServerHost}:${this.schedulerConfig.artifactServerPort}/resource/$path"))
+      .addUris(CommandInfo.URI.newBuilder.setValue(s"http://${this.schedulerConfig.artifactServerHost}:${this.schedulerConfig.artifactServerPort}/resource/$executorConfigPath"))
+      .addUris(CommandInfo.URI.newBuilder.setValue(s"http://${this.schedulerConfig.artifactServerHost}:${this.schedulerConfig.artifactServerPort}/resource/$producerConfigPath"))
       .setValue(cmd))
       .setName("LogLine Transform Executor")
       .setSource("cisco")
@@ -218,8 +135,6 @@ object Scheduler extends org.apache.mesos.Scheduler {
   }
 }
 
-private case class SchedulerConfig(master: String = "", user: String = "root", var instances: Int = 1,
-                                   artifactServerHost: String = "master", artifactServerPort: Int = 6666,
-                                   executor: String = "", cpuPerTask: Double = 0.2, memPerTask: Double = 256,
-                                   producerConfig: String = "", topic: String = "",
-                                   dropwizardConfig: String = "config.yml", executorDropwizardConfig: String = "executor.yml")
+private case class SchedulerConfig(master: String = "", user: String = "root", artifactServerHost: String = "master", artifactServerPort: Int = 6666,
+                                   executor: String = "", producerConfig: String = "", topic: String = "",
+                                   dropwizardConfig: String = "config.yml", executorDropwizardConfig: String = "executor.yml") extends ConfigBase
