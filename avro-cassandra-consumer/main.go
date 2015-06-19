7e92b5b60ac0ac494617967117e10bdc729be384
ilyutov <ivanlyutov@gmail.com>
2015-06-19 13:18:21 +0300

    Improved flags handling.

--- avro-cassandra-consumer/main.go
+++ avro-cassandra-consumer/main.go
@@ -10,30 +10,34 @@ import (
     kafka "github.com/stealthly/go_kafka_client"
 )
 
-var cassandraHost = flag.String("cassandra.host", "localhost", "Cassandra host")
-var cassandraKeyspace = flag.String("cassandra.keyspace", "spark_analysis", "Cassandra keyspace")
-var topics = flag.String("topics", "", "Comma separated kafka topics to read from")
-var numStreams = flag.Int("num.streams", 1, "Amount of streams for each topic")
-var group = flag.String("group", "ac-consumer", "Kafka consumer group name")
-var zkConnect = flag.String("zookeeper", "localhost:2181", "Zookeeper host:port")
-var schemaRegistryUrl = flag.String("schema.registry.url", "http://localhost:8081", "Schema registry URL")
-var updateInterval = flag.String("update.interval", "1s", "Interval at which Cassandra should be updated")
-var logLevel = flag.String("log.level", "info", "Log level")
+var flags = flag.NewFlagSet("Kafka Avro Consumer", flag.ExitOnError)
+var cassandraHost = flags.String("cassandra.host", "", "Cassandra host --cassandra.host [host]")
+var cassandraKeyspace = flags.String("cassandra.keyspace", "spark_analysis", "Cassandra keyspace")
+var topics = flags.String("topics", "", "Comma separated list of kafka topics: --topics [topic1,topic2...]")
+var numStreams = flags.Int("num.streams", 1, "Amount of streams for each topic")
+var group = flags.String("group", "ac-consumer", "Kafka consumer group name")
+var zkConnect = flags.String("zookeeper", "", "Zookeeper connection string: --zookeeper [host:port]")
+var schemaRegistryUrl = flags.String("schema.registry.url", "", "Avro Schema Registry URL: --schema.registry.url http://[host:port]")
+var updateInterval = flags.Duration("update.interval", 1 * time.Second, "Interval at which Cassandra should be updated")
+var logLevel = flags.String("log.level", "info", "Log level")
 
 func main() {
-	flag.Parse()
-	if *topics == "" {
-		log.Fatal("You have to provide at least one topic")
-	}
-
-	parsedInterval, err := time.ParseDuration(*updateInterval)
-	if err != nil {
-		log.Fatalf("Incorrect update interval argument: %s", *updateInterval)
-	}
-
+	flags.Parse(os.Args[1:])
     if logLevel != nil {
         setLogLevel(*logLevel)
     }
+	if *cassandraHost == "" {
+		log.Fatal("You have to provide Cassandra host: --cassandra.host [host]")
+	}
+	if *zkConnect == "" {
+		log.Fatal("You have to provide at least one zookeeper host: --zookeeper [host:port]")
+	}
+	if *schemaRegistryUrl == "" {
+		log.Fatal("You have to provide Schema Registry URL: --schema.registry.url http://[host:port]")
+	}
+	if *topics == "" {
+		log.Fatal("You have to provide at least one topic: --topics [topic1,topic2...]")
+	}
 
 	config := AvroCassandraConsumerConfig{
 		CassandraHost:     *cassandraHost,
@@ -43,7 +47,7 @@ func main() {
 		Group:             *group,
 		ZkConnect:         strings.Split(*zkConnect, ","),
 		SchemaRegistryUrl: *schemaRegistryUrl,
-		UpdateInterval:    parsedInterval,
+		UpdateInterval:    *updateInterval,
 	}
 	acConsumer := NewAvroCassandraConsumer(&config)
 
